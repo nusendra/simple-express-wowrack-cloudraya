965472f5fb9b38b78050895b72ff61f25229a769
Nusendra Hanggarawan <admin@nusendra.com>
2024-03-22 23:54:54 +0700

    fix: default value for port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
-const port = process.env.PORT | 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
