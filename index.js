0e3734049cce91191eb87c68ceb7bc0ed3c0d3bc
Nusendra Hanggarawan <admin@nusendra.com>
2024-03-23 17:11:23 +0700

    add new route

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/wowrack', (req, res) => {
+  res.send('selamat sore!')
+})
+
 app.get('/test', (req, res) => {
   res.send('test 123')
 })
