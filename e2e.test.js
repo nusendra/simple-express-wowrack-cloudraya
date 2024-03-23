123c6ca18471ab650ea15278824b77cd1d91dd54
Nusendra Hanggarawan <admin@nusendra.com>
2024-03-23 17:16:13 +0700

    fix test

--- e2e.test.js
+++ e2e.test.js
@@ -3,7 +3,7 @@ const app = require('./index');
 
 describe('GET /wowrack', () => {
   it('response text `test 123`', async () => {
-    const res = await request(app).get('/test');
+    const res = await request(app).get('/wowrack');
 
     expect(res.statusCode).toBe(200);
     expect(res.text).toBe("selamat sore!");
