c610bf6f7973af72f3749622792d9299811f4d73
Nusendra Hanggarawan <admin@nusendra.com>
2024-03-23 17:13:53 +0700

    change test case

--- e2e.test.js
+++ e2e.test.js
@@ -1,12 +1,12 @@
 const request = require('supertest');
 const app = require('./index');
 
-describe('GET /test', () => {
+describe('GET /wowrack', () => {
   it('response text `test 123`', async () => {
     const res = await request(app).get('/test');
 
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe("test 123");
+    expect(res.text).toBe("selamat pagi");
   });
 });
 
